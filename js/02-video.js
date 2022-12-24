71ea75d4d585b288bb4b2c4be4d51be036512ca4
Anna Volfman <aniya_07@ukr.net>
2022-12-24 19:53:01 +0200

    update form

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,6 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+localStorage.setItem(STORAGE_KEY, 'xnvvjxn')
+console.log(localStorage.getItem('videoplayer-current-time'))
--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input');
+const textAreaEl = document.querySelector('textArea');
+
+populateTextArea();
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit (event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    let dataSrtring = JSON.stringify(formData);
+    localStorage.setItem(STORAGE_KEY, dataSrtring);
+}
+
+function populateTextArea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        let dataObject = JSON.parse(savedMessage);
+        emailEl.value = dataObject.email;
+        textAreaEl.value = dataObject.message;
+    }
+}
 
